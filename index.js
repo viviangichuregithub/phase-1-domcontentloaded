b773a023c53875fce83247c1341ae42555287f14
Vivian Gichure <viviangichure@gmail.com>
2025-06-12 21:25:12 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const paragraph = document.getElementById("text");
+  paragraph.textContent = "This is really cool!";
+});
+console.log("Before DOMContentLoaded fires");
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM fully loaded!");
+});
