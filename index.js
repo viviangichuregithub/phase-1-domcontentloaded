1ae4ddbd412dbb0ed12dccb1d54aafaed45c238f
Vivian Gichure <viviangichure@gmail.com>
2025-06-12 21:55:01 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Your code goes here
+// Fires immediately when index.js loads
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+
+// Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+
+  // Select the paragraph with id="text" and change its content
   const paragraph = document.getElementById("text");
   paragraph.textContent = "This is really cool!";
-});
-console.log("Before DOMContentLoaded fires");
-
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM fully loaded!");
 });
